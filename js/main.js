e4654a3092ca3f094c43930c040653da491c80df
gelicia <kdurivage@gelicia.com>
2013-06-01 14:51:22 -0500

    search enhance

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
+function startSearch(){
+	$("#legSearchBtn").attr("disabled", "disabled");
+	var searchPromise = search();
+
+	searchPromise.done(function(){
+		$("#legSearchBtn").removeAttr("disabled");
+	});
+}
+
 function search () {
+	var def = $.Deferred();
+
 	$("[id^='results']").remove();
 	var searchQuery = $("#legSearch")[0].value;
 
 	var searchURL = 'http://openstates.org/api/v1/bills/?state=mn&q=' + searchQuery + '&apikey=e74b1e987eec45f8ba07ef58c54a231a';
 
    $.getJSON(searchURL + "&callback=?", null, function(bills) {
-		console.log(bills); 
+		console.log(bills);
+		def.resolve(); 
     });
+
+   return def.promise();
 }
