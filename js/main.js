ef4ee1796c4f95e6fad075855858312484cf5ebd
gelicia <kdurivage@gelicia.com>
2013-06-01 14:10:26 -0500

    Search works

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-function loadDataset () {
-	
+function search () {
+	$("[id^='results']").remove();
+	var searchQuery = $("#legSearch")[0].value;
+
+	var searchURL = 'http://openstates.org/api/v1/bills/?state=mn&q=' + searchQuery + '&apikey=e74b1e987eec45f8ba07ef58c54a231a';
+
+   $.getJSON(searchURL + "&callback=?", null, function(bills) {
+		console.log(bills); 
+    });
 }
